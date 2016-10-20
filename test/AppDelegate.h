884740189319bcda552200867cfcfcd99311113c
1658786291 <1658786291@qq.com>
2016-10-20 13:48:10 +0800

    text
    text

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Aisino on 16/10/20.
+//  Copyright © 2016年 Aisino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Aisino on 16/10/20.
+//  Copyright © 2016年 Aisino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
